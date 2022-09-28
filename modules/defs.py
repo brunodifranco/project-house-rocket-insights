ddf535dad4ab11919c79da059be143ce080a4c6d
Bruno Di Franco Albuquerque <brunodifranco99@gmail.com>
2022-09-28 18:49:22 -0300

    Update defs.py

--- modules/defs.py
+++ modules/defs.py
@@ -5,13 +5,13 @@ geolocator = Nominatim(user_agent='geo_api_exe')
 
 def get_adress(x):
    index, row = x
-   time.sleep(1) # Tempo entre uma requisição e outra, para não sobrecarregar a API
-   response = geolocator.reverse(row['query']) # Passa a query
-   address = response.raw['address'] # Chamada API
+   time.sleep(1) 
+   response = geolocator.reverse(row['query']) # query
+   address = response.raw['address'] # API call
 
-   try: # Pega os valores da requisição e retorna       
-       road = address['road'] if 'road' in address else 'NA'
-       house_number = address['house_number'] if 'house_number' in address else 'NA'
+   try:     
+       road = address['road'] if 'road' in address else 'NA' # getting the road info
+       house_number = address['house_number'] if 'house_number' in address else 'NA' # getting the house number info
        
        return road, house_number
    except:
